e1230b9f3f46ef4ce1f92902e11aaf1834b46f14
Andreu <64242440+Pyroster@users.noreply.github.com>
2021-05-29 05:41:16 +0200

    added basic calculator controls

--- LeyCalc.js
+++ LeyCalc.js
@@ -1,85 +1,144 @@
-/*calculadora leyendas*/
 const calculator = {
-  displayValue: "0"
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
 };
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+}
+
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+  	calculator.displayValue = "0."
+    calculator.waitingForSecondOperand = false;
+    return
+  }
+
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+  
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+
+  if (firstOperand == null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === '*') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+function resetCalculator() {
+    pdesReset();
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
 
 function updateDisplay() {
   // select the element with class of `calculator-screen`
-  const display = document.querySelector(".calculator-screen");
+  const display = document.querySelector(".scrn2");
   // update the value of the element with the contents of `displayValue`
   display.value = calculator.displayValue;
 }
 
 updateDisplay();
 
-const keys = document.querySelector(".calculator-keys");
-keys.addEventListener("click", (event) => {
-  // Access the clicked element
+const keys = document.querySelector(".keypad");
+keys.addEventListener('click', event => {
   const { target } = event;
-
-  // Check if the clicked element is a button.
-  // If not, exit from the function
-  if (!target.matches("button")) {
-    return;
-  }
-
-  if (target.classList.contains("convert")) {
+  const { value } = target;
+    if (target.classList.contains("convert")) {
     leyCalc();
     return;
   }
-
-  if (target.classList.contains("all-clear")) {
-    resetCalculator();
-    updateDisplay();
-
+  if (!target.matches('button')) {
     return;
   }
 
-  inputDigit(target.value);
+  switch (value) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+      handleOperator(value);
+      break;
+    case '.':
+      inputDecimal(value);
+      break;
+    case 'all-clear':
+      resetCalculator();
+      break;
+    default:
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
+  }
+
   updateDisplay();
 });
 
-function inputDigit(digit) {
-  const { displayValue } = calculator;
-  // Overwrite `displayValue` if the current value is '0' otherwise append to it
-  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
-}
-
-function resetCalculator() {
-  calculator.displayValue = "0";
-  console.log(calculator);
-  allclear();
+function pdesReset() {
+  document.querySelector(".scrn").value = '0';
 }
 
 function leyCalc() {
-  var num1 = document.getElementById("calc-screen").value;
+  var num1 = document.getElementById("pdesscrn").value;
   if (num1 == 0) {
-    num1 = document.getElementById("perc-display").innerHTML = -25;
+    num1 = document.querySelector(".scrn").value = -25;
   } else if (51 <= num1 && num1 <= 90) {
-    num1 = document.getElementById("perc-display").innerHTML = (
-      50 +
-      (num1 - 50) / 2
-    );
+    num1 = document.querySelector(".scrn").value =
+      50 + (num1 - 50) / 2;
   } else if (91 <= num1 && num1 <= 150) {
-    num1 = document.getElementById("perc-display").innerHTML = (
-      70 +
-      (num1 - 90) / 3
-    ).toFixed(1);
+    num1 = document.querySelector(".scrn").value =
+      70 + (num1 - 90) / 3;
   } else if (151 <= num1 && num1 <= 190) {
-    num1 = document.getElementById("perc-display").innerHTML = (
-      90 +
-      (num1 - 150) / 4
-    ).toFixed(1);
+    num1 = document.querySelector(".scrn").value =
+      90 + (num1 - 150) / 4;
   } else if (191 <= num1) {
-    num1 = document.getElementById("perc-display").innerHTML = (
-      190 +
-      (num1 - 190) / 5
-    ).toFixed(1);
+    num1 = document.querySelector(".scrn").value =
+      190 + (num1 - 190) / 5;
   } else {
-    num1 = document.getElementById("perc-display").innerHTML = num1;
+    num1 = document.querySelector(".scrn").value = num1;
   }
   return;
-}
-function allclear() {
-  document.getElementById("perc-display").innerHTML = "0";
 }
